aa64077ca00dd5b5181c354b4cabc839e4b39237
Carla Valdivia <valdivia.carla@gmail.com>
2023-02-24 12:46:44 -0300

    restaurant + review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+  end
+  resources :reviews, only: [:destroy]
 end
